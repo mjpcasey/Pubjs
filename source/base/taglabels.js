2f2183a61a223ad3e5e5bff9e2733ade681ee529
陈璐 <chenlu@sunteng.com>
2015-01-23 17:58:31 +0800

    Update: 修改taglabels的reset方法

--- source/base/taglabels.js
+++ source/base/taglabels.js
@@ -726,6 +726,12 @@ define(function(require, exports){
 		reset: function(){
 			this.$innerBody.empty();
 			this.$type = -1;
+
+			var c = this.getConfig();
+			if (c.data) {
+				this.setData(c.data);
+			}
+			return this;
 		},
 		setData: function(data){
 			this.reset();
